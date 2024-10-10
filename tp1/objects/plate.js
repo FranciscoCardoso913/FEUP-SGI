70076f182e61a23eccea9546cbbc9a2e75e964c5
FranciscoCardoso915 <franciscocardoso.3003@gmail.com>
2024-10-10 09:49:02 +0100

    arrange scene

--- tp1/objects/plate.js
+++ tp1/objects/plate.js
@@ -20,7 +20,7 @@ export class Plate {
             shininess: 90
         })
 
-        let cylinder = new THREE.CylinderGeometry(1.1, 1, 0.1, 10,10);
+        let cylinder = new THREE.CylinderGeometry(1.8, 1.5, 0.1, 15,15);
         this.cylinderMesh = new THREE.Mesh(cylinder, cylinderMaterial);
         this.cylinderMesh.position.set(this.position.x, this.position.y + 0.05, this.position.z)
 
