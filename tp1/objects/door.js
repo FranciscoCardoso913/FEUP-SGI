f39836d68e8df7de7d12a74c1a15e128310a8dd9
Francisco Cardoso <up202108793@up.pt>
2024-10-29 15:11:19 +0000

    Making a door

---
+++ tp1/objects/door.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+
+export class Door{
+
+    constructor(scene ,position = new THREE.Vector3(0,0,0),width=5,height=12) {
+        this.scene = scene
+        this.width=width
+        this.height=height
+        this.position = position
+
+        this.init()
+    }
+
+    init() {
+        const geometry = new THREE.PlaneGeometry(this.width,this.height,5,5)
+        let map =new THREE.TextureLoader().load( 'textures/door.jpg' );
+
+        map.wrapS = map.wrapT = THREE.RepeatWrapping;
+
+        map.anisotropy = 4;
+
+        map.colorSpace = THREE.SRGBColorSpace;
+
+        const material = new THREE.MeshLambertMaterial( { map: map, color:0xfebc99} );
+        this.door = new THREE.Mesh(geometry, material)
+        this.door.position.add(new THREE.Vector3(0, this.height/2, 0))
+        this.door.position.add(this.position)
+        this.door.rotateY(Math.PI/2)
+
+    }
+
+    enable(){
+        this.scene.add(this.door)
+    }
+    
+    disable(){
+        this.scene.remove(this.door)
+    }
+}
