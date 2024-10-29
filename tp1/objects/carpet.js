3e5bbf21d36f2d82c77f357890cff07f7b048318
FranciscoCardoso915 <franciscocardoso.3003@gmail.com>
2024-10-29 01:54:13 +0000

    Adding carpet

---
+++ tp1/objects/carpet.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+
+export class Carpet{
+
+    constructor(scene ,width=10,height=15,position = new THREE.Vector3(0,0,0)) {
+        this.scene = scene
+        this.width=width
+        this.height=height
+        this.position = position
+
+        this.init()
+    }
+
+    init() {
+        const geometry = new THREE.PlaneGeometry(this.width,this.height,5,5)
+        let map =new THREE.TextureLoader().load( 'textures/carpet.jpg' );
+
+        map.wrapS = map.wrapT = THREE.RepeatWrapping;
+
+        map.anisotropy = 4;
+
+        map.colorSpace = THREE.SRGBColorSpace;
+
+        const material = new THREE.MeshLambertMaterial( { map: map, color:0xcccccc} );
+        this.carpet = new THREE.Mesh(geometry, material)
+        this.carpet.position.add(this.position)
+        this.carpet.rotateX(-Math.PI/2)
+
+    }
+
+    enable(){
+        this.scene.add(this.carpet)
+    }
+}
