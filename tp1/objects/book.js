98b128d092c757a07b7a9aec079a329706751419
Francisco Cardoso <up202108793@up.pt>
2024-10-29 02:34:38 +0000

    Adding books and shelves to the scene

--- tp1/objects/book.js
+++ tp1/objects/book.js
@@ -104,7 +104,7 @@ export class Book{
           return mesh
     }
 
-    activate(){
+    enable(){
 
         this.scene.add( this.book )
     }
--- tp1/objects/shelve.js
+++ tp1/objects/shelve.js
@@ -73,7 +73,7 @@ export class Shelve{
         
     }
 
-    activate(){
+    enable(){
         this.scene.add(this.shelve)
     }
 
