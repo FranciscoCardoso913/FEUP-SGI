feb43b5da340d74201549c1003434c1a6412ad8f
FranciscoCardoso915 <franciscocardoso.3003@gmail.com>
2024-11-28 22:38:24 +0000

    Materials parser

--- tp2/parser/MyGraph.js
+++ tp2/parser/MyGraph.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import {rgbToHex} from './utils.js'
-import {parseAmbientLight,parseFog, parseSkybox} from './parser.js'
+import {parseAmbientLight,parseFog, parseSkybox,parseTextures,parseMaterials} from './parser.js'
 
 
 class MyGraph {
@@ -14,7 +14,9 @@ class MyGraph {
 	}
     init(){
        this.parseGlobals(this.json['globals'])
-	   this.parseTextures(this.json['textures'])
+	   this.textures= parseTextures(this.json['textures'])
+	   this.materials = parseMaterials(this.textures, this.json["materials"])
+
     }
 
 	parseGlobals(globals){
@@ -23,34 +25,7 @@ class MyGraph {
 		this.fog = parseFog(globals["fog"])
 		this.skybox = parseSkybox(globals['skybox'])
 	}
-	parseTextures(textures){
-		this.textures = Object.entries(textures).reduce((dict, [name, value]) => {
-			let texture;
-			if(value.isVideo){
-		
-				const video = document.createElement('video');
-				video.src = value.filepath; 
-				video.loop = true; 
-				video.muted = true; 
-				video.play(); 
-				texture = new THREE.VideoTexture(video);
-		
-			}
-			else{
-				texture = new THREE.TextureLoader().load(value.filepath)
-				texture.minFilter = THREE.LinearMipmapLinearFilter; 
-				texture.mipmaps = []
-				for( let i =0; i<= 7;i++){
-					if(value["mipmap"+i.toString()]){
-						texture.mipmaps.append(value["mipmap"+i.toString()]);
-					}else break;
-				}
 
-			}
-			dict[name] = texture;
-			return dict;
-		}, {});
-	}
 	parseMaterials(materials){
 
 	}
