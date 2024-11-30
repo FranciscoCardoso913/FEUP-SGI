65b951d3bdf6988fc84c18be84e90c6f007b5669
Francisco Cardoso <up202108793@up.pt>
2024-11-30 13:01:55 +0000

    Adding rotate transformation

--- tp2/parser/utils.js
+++ tp2/parser/utils.js
@@ -4,6 +4,10 @@ export function rgbToHex(rgb) {
         const hex = Math.round(value * 255).toString(16);
         return hex.padStart(2, '0');
     };
-
+    console.log(`#${toHex(rgb.r)}${toHex(rgb.g)}${toHex(rgb.b)}`)
     return `#${toHex(rgb.r)}${toHex(rgb.g)}${toHex(rgb.b)}`;
+}
+
+export function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180);
 }
