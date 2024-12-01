676ee1c94fd17eeb203e9c74be107d47be91728a
FranciscoCardoso915 <franciscocardoso.3003@gmail.com>
2024-12-01 02:07:40 +0000

    Build directionallight

--- tp2/parser/utils.js
+++ tp2/parser/utils.js
@@ -4,7 +4,6 @@ export function rgbToHex(rgb) {
         const hex = Math.round(value * 255).toString(16);
         return hex.padStart(2, '0');
     };
-    console.log(`#${toHex(rgb.r)}${toHex(rgb.g)}${toHex(rgb.b)}`)
     return `#${toHex(rgb.r)}${toHex(rgb.g)}${toHex(rgb.b)}`;
 }
 
