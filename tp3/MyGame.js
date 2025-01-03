5989d5143b50903ceaea36fb6084eca4f406833f
FranciscoCardoso915 <franciscocardoso.3003@gmail.com>
2025-01-03 22:49:10 +0000

    Animation

--- tp3/MyGame.js
+++ tp3/MyGame.js
@@ -4,6 +4,7 @@ import { MyFileReader } from './parser/MyFileReader.js';
 import { MyGraph } from './parser/MyGraph.js';
 import { MyGuiInterface } from './MyGuiInterface.js';
 import MyBallon from './factories/MyBalloon.js';
+import animate from './animation.js';
 /**
  *  This class contains the contents of out application
  */
@@ -17,9 +18,10 @@ class MyGame {
         PICKING: 0,
     };
 
-    constructor(scene){
+    constructor(app){
+       
         this.fps = 30
-        this.scene = scene
+        this.scene = app.scene
         this.ballons = [
             new MyBallon(0xff3333),
             new MyBallon(0x33ff33),
@@ -67,23 +69,33 @@ class MyGame {
 
         let selected = 0
 
+        ballons.forEach((ballon)=>{
+            this.scene.add(ballon.getObject())
+        })
+
         function drawBallons(scene,ballons, selected){
+          
             let r = 3
             let n = ballons.length
             for (let i = 0; i < n; i++){
                 let ballon = ballons[i]
                 let theta = 2 * (Math.PI/n) * (i - selected) 
-                scene.remove(ballon.getObject())
-                ballon.setPosition(new THREE.Vector3(r * Math.sin(-theta),1, r * Math.cos(-theta)))
-                scene.add(ballon.getObject())
+                //scene.remove(ballon.getObject())
+                let keyframes = ballon.move(new THREE.Vector3(r * Math.sin(-theta),1, r * Math.cos(-theta)))
+     
+                animate(ballon.getObject(), keyframes, Date.now(), 1)
             }
         }
 
         function sleep(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
           }
+
+       
   
         drawBallons(this.scene,ballons,selected)
+     
+     
         let isSelected = false
         while(!isSelected){
             let right = this.hasBeenPressedKeys["ArrowRight"] || false;
@@ -99,13 +111,14 @@ class MyGame {
                 selected = n -1
 
             if (left || right || isSelected){
-                drawBallons(this.scene,ballons,selected)
+                drawBallons(this.scene,ballons,selected )
                 this.hasBeenPressedKeys = {}
             }
             
             await sleep(1000/this.fps)
             
         }
+      
         ballons.forEach(ballon => {
             this.scene.remove(ballon.getObject())
         });
